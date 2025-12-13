56216cf224fc8784b32f4ce862449630594fc77f
Maelle Danset <maelledanset@gmail.com>
2025-12-13 12:37:16 +0100

    finish 2025/09 part1
    Co-authored-by: Ulysse Buonomo <buonomo.ulysse@gmail.com>

--- 2025/09/part1.py
+++ 2025/09/part1.py
@@ -4,7 +4,32 @@ from sys import argv
 
 # 50
 
-def parse() -> ...:
+
+def parse() -> list[tuple[int, int]]:
+	rv = []
 	with open(argv[1], 'r') as file:
 		for line in file:
-			return line.strip()
+			a, b = line.split(',')
+			rv.append((int(a), int(b)))
+		return rv
+
+
+def size(a: tuple[int, int], b: tuple[int, int]) -> int:
+	"""
+	>>> size((3, 2), (1, 1))
+	6
+	"""
+	a1, a2 = a
+	b1, b2 = b
+	return (abs(b1 - a1) + 1) * (abs(b2 - a2) + 1)
+
+
+if __name__ == '__main__':
+	tiles = parse()
+	largest = 0
+	for i, t1 in enumerate(tiles):
+		for t2 in tiles[i + 1 :]:
+			area = size(t1, t2)
+			if area > largest:
+				largest = area
+	print(largest)
